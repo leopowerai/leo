d3dca78388e63249b7aba9496c9c387c686c5eca
cirogam22 <cirogam@tclabs.systems>
2024-10-29 17:49:08 -0500

    Added clases and test for pbi assignment

--- Backend/local_main.py
+++ Backend/local_main.py
@@ -1,3 +1,4 @@
+from pbi_assigner import Complexity, PBIAssigner, ProductBacklogItem
 from profile_generator import Student
 from project_assigner import Project, ProjectAssigner
 
@@ -10,6 +11,25 @@ def main():
             name="Test Project 1", stack=["Python", "Django", "JavaScript", "React"]
         )
         projects = [project1]
+
+        # Project PBIs
+        pbi1 = ProductBacklogItem(
+            title="Tarea 1, de Python",
+            description="Esta es la tarea de prueba 1",
+            complexity=Complexity.LOW,
+            stack=["Python", "OpenAI"],
+            project=project1,
+        )
+
+        pbi2 = ProductBacklogItem(
+            title="Tarea 2, de JS",
+            description="Esta es la tarea de prueba 2",
+            complexity=Complexity.LOW,
+            stack=["JavaScript", "OpenAI"],
+            project=project1,
+        )
+
+        pbis = [pbi1, pbi2]
 
         # Get the student
         platzi_url = "https://platzi.com/p/LuisManuel/"
@@ -26,6 +46,21 @@ def main():
                 print(
                     f"Match de proyecto {project_match[0].name} con {project_match[1]} tech, para el estudiante {student.platzi_username}"
                 )
+
+                # Product Backlog Item assign
+                pbi_assigner = PBIAssigner(
+                    student=student,
+                    project=project1,
+                    project_match_techs=[project_match[1]],
+                    pbis=pbis,
+                )
+
+                assigned_pbi = pbi_assigner.assign_pbi_to_student()
+                if assigned_pbi:
+                    print(f"Se asignó el ticket {assigned_pbi.title}")
+                else:
+                    print("No se encontró ticket para asignar")
+
             else:
                 print("No hubo match de proyecto")
 
