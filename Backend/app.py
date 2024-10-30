9af6c3d4abe79348829e1276578be0437360c4c4
alvarojhr <alvarojhr96@gmail.com>
2024-10-30 14:37:48 -0500

    Endpoints and integration with front

--- Backend/app.py
+++ Backend/app.py
@@ -64,5 +64,61 @@ def submit():
         return jsonify({"message": "Error procesando la solicitud"}), 500
 
 
+@app.route("/unassign", methods=["POST"])
+def unassign():
+    data = request.get_json()
+    student_username = data.get("username")
+
+    if not student_username:
+        return jsonify({"error": "El campo 'username' es requerido"}), 400
+
+    # Check from the database if the student has an assignment
+    if True:#db.assignment_exists(student_username):
+        # Remove the assignment from the database
+        #db.delete_assignment(student_username)
+        logging.info(f"Student {student_username} has been unassigned from their project")
+        return jsonify({"message": f"El estudiante {student_username} ha sido desasignado del proyecto"}), 200
+    else:
+        logging.info(f"Student {student_username} has no assigned project to unassign")
+        return jsonify({"error": f"El estudiante {student_username} no tiene un proyecto asignado"}), 400
+
+
+@app.route("/update_status", methods=["POST"])
+def update_status():
+    data = request.get_json()
+    student_username = data.get("username")
+    new_status = data.get("status")
+
+    if not student_username or not new_status:
+        return jsonify({"error": "Los campos 'username' y 'status' son requeridos"}), 400
+
+    # Retrieve from the database the assignment
+    #assignment = db.get_assignment(student_username)
+    if True:
+        # Set new status
+        #assignment.pbi.status = new_status
+        logging.info(f"Student {student_username} assignment status updated to {new_status}")
+        return jsonify({
+            "message": f"El estado del proyecto asignado al estudiante {student_username} ha sido actualizado a {new_status}"
+        }), 200
+    else:
+        logging.info(f"Student {student_username} has no assigned project to update")
+        return jsonify({"error": f"El estudiante {student_username} no tiene un proyecto asignado"}), 400
+
+
+@app.route("/has_in_progress/<username>", methods=["GET"])
+def has_in_progress(username):
+    # Retrieve from the database the assignment
+    #assignment = db.get_assignment(username)
+    if True:
+        if True: #assignment.pbi.status == "In Progress":
+            return jsonify({"has_in_progress": True}), 200
+        else:
+            return jsonify({"has_in_progress": False}), 200
+    else:
+        return jsonify({"error": f"El estudiante {username} no tiene un proyecto asignado"}), 400
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
